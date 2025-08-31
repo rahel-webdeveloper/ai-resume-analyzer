86e3b635ca6c8ff30f64fd084a0860bdfecbf4fe
Khatibullah Rahel <khatibullahrahel25@gmail.com>
2025-08-31 07:12:56 +0430

    Create sign in and log out functionality and protect routes from unauthenticated person

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,16 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+const meta = () => {
+  [
+    { title: "AI Resume Analyzer | Auth" },
+    {
+      name: "description",
+      content: "Authenticate to access the AI Resume Analyzer application.",
+    },
+  ];
+};
+
+export default [
+  index("routes/home.tsx"),
+  route("/auth", "routes/auth.tsx"),
+] satisfies RouteConfig;
